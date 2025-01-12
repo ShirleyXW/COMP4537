ad1099046fcc3885dbfbdb31cb6caba6a3d248b0
ShirleyXW <shirley.wang.xl@outlook.com>
2025-01-11 21:46:24 -0800

    create User class

---
+++ labs/0/lang/messages/en/user.js
@@ -0,0 +1,14 @@
+class User {
+    getInvalidText() {
+        return "Please enter a number between 3 and 7";
+    }
+    getCongratsText() {
+        return "Excellent memory!";
+    }
+    getGameOverText() {
+        return "Wrong order!";
+    }
+    getSuccessText() {
+        return "Buttons created!";
+    }
+}
