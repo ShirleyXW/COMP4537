dab3895f95f1f1476401b735ec6d61466973a96d
ShirleyXW <shirley.wang.xl@outlook.com>
2025-01-12 09:46:34 -0800

    make buttons shuffle

--- labs/0/js/script.js
+++ labs/0/js/script.js
@@ -2,19 +2,31 @@
 
 let colorExists = [];
 let buttons = [];
+const shuffleInterval = 2000;
+const defaultFontSize = 16;
+const btnWidth = 10;
+const btnHeight = 5;
 const userText = new User();
 
 class Button {
     constructor(id) {
         this.btn = document.createElement("button");;
         this.btn.id = "btn" + id;
+        this.btn.className = "btn";
         this.btn.innerText = id;
-        this.btn.style.width = "10em";
-        this.btn.style.height = "5em";
+        this.btn.style.width = `${btnWidth}em`;
+        this.btn.style.height = `${btnHeight}em`;
         this.btn.style.backgroundColor = getColor();
     }
     getButton() {
         return this.btn;
+    }
+    getRandomPosition() { 
+        const maxX = document.getElementById("playground").offsetWidth - btnWidth * defaultFontSize;
+        const maxY = document.getElementById("playground").offsetHeight - btnHeight * defaultFontSize;
+        let left = Math.floor(Math.random() * maxX);
+        let top = Math.floor(Math.random() * maxY);
+        return [left, top];
     }
 }
 
@@ -22,6 +34,7 @@ class Playground {
     constructor() {
         this.playground = document.createElement("section");
         this.playground.id = "playground";
+        this.playground.position = "relative";
     }
     clearPlayground() {
         this.playground.innerHTML = "";
@@ -67,12 +80,29 @@ function addBtns(numOfBtns) {
     }
 }
 
+function shuffle(number) {
+    let intervalID = setInterval(()=>{
+        buttons.forEach((button) => {
+            button.btn.innerText="";
+            button.btn.style.position = "absolute";
+            let position = button.getRandomPosition();
+            let left = position[0];
+            let top = position[1];
+            button.btn.style.left = left + "px";
+            button.btn.style.top = top + "px";
+        });
+    }, shuffleInterval);
+    setTimeout(() => clearInterval(intervalID), shuffleInterval * number);
+}
+
 function go() {
     let numOfBtns = document.getElementById("number").value;
     let result;
     if (isValidInput(numOfBtns)) {
-        addBtns(parseInt(numOfBtns));
+        let number = parseInt(numOfBtns);
+        addBtns(number);
         result = userText.getSuccessText();
+        setTimeout(shuffle(number), number*1000);
     } else {
         result = userText.getInvalidText();
         alert(result);
