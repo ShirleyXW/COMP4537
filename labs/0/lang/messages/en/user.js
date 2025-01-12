2c40979f92cc26d6de64e8e690e65151247a50b4
ShirleyXW <shirley.wang.xl@outlook.com>
2025-01-12 12:57:28 -0800

    encapsulate messages

--- labs/0/lang/messages/en/user.js
+++ labs/0/lang/messages/en/user.js
@@ -1,17 +1,25 @@
+function Message() {
+    this.invalidText = "Please enter a number between 3 and 7";
+    this.congratsText = "Excellent memory!";
+    this.gameOverText = "Wrong order!";
+    this.successText = "Buttons created!";
+}
+
 class User {
     constructor(playground) {
         this.playground = playground;
+        this.message = new Message();
     }
     getInvalidText() {
-        return "Please enter a number between 3 and 7";
+        return this.message.invalidText;
     }
     getCongratsText() {
-        return "Excellent memory!";
+        return this.message.congratsText;
     }
     getGameOverText() {
-        return "Wrong order!";
+        return this.message.gameOverText;
     }
     getSuccessText() {
-        return "Buttons created!";
+        return this.message.successText;
     }
 }
