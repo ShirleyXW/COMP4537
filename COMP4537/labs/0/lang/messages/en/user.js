b709bec1320a162f650fb5c8d2281b0d7d209ec1
ShirleyXW <shirley.wang.xl@outlook.com>
2025-01-12 16:20:17 -0800

    remove unnecessary property

--- COMP4537/labs/0/lang/messages/en/user.js
+++ COMP4537/labs/0/lang/messages/en/user.js
@@ -1,6 +1,5 @@
 class User {
-    constructor(playground, message) {
-        this.playground = playground;
+    constructor(message) {
         this.message = message;
     }
     getInvalidText() {
